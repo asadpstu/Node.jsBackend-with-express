8da5773155edc64794cfced6931e5b9820e777fd
Asad <hmasad09@gmail.com>
2018-11-20 00:11:09 +0800

    GET-2/POST/PATCH/DELETE all done

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const morgan  = require('morgan');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+
+
+
 //Mongo db connection
 mongoose.connect('mongodb://root:12345@cluster0-shard-00-00-ecaim.mongodb.net:27017,cluster0-shard-00-01-ecaim.mongodb.net:27017,cluster0-shard-00-02-ecaim.mongodb.net:27017/Laravel-vue-song?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
 );
--- route/product.js
+++ route/product.js
@@ -4,29 +4,45 @@ const router = express.Router();
 const song = require('../models/Song');
 const mongoose = require('mongoose');
 
+
+
 // Create product Route GET/POST/PATCH/DELETE
 //Route initialization done on app.js
+
+// http://localhost:3000/product/  Get Method {all product}
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-      message : "Product / Get request"
+    song.find()
+    .exec()
+    .then(doc =>{
+       console.log(doc);
+       res.status(200).json({doc});
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({err:err});
     });
 });
 
+
+// http://localhost:3000/product   Post Method
 router.post('/',(req,res,next)=>{
 
-    // const newproduct = {
-    //     songname : req.body.song,
-    //     singer_id : req.body.singer_id,
-    //     song_type : req.body.song_type,
-    //     song_duration : req.body.duration
-    // };
+/*
+Data Format for client  request method body
+{
+    "song": "Amar Sombol",
+    "singer_id": 17,
+    "song_type": "Folk Song",
+    "song_duration": 5.9
+}
+*/
 
     const newsong = new song({
         _id : new mongoose.Types.ObjectId,
         songname : req.body.song,
         singer_id : req.body.singer_id,
         song_type : req.body.song_type,
-        song_duration : req.body.duration
+        song_duration : req.body.song_duration
     });
     newsong.save().then(result =>{
         console.log(result);
@@ -40,32 +56,73 @@ router.post('/',(req,res,next)=>{
     });
 });
 
+// http://localhost:3000/product/{id}   Get Method
 router.get('/:productid',(req,res,next)=>{
 
     const id = req.params.productid;
-    if(id == 'SPECIAL')
-    {
-        res.status(200).json({
-          message : "You are requesting for Special Product"
-        });
-    }
-    else
-    {
-        res.status(200).json({
-            message : "Specific Product -"+id
-          });
-    }
-});
-
-router.patch('/',(req,res,next)=>{
-    res.status(200).json({
-      message : "Product / Update Request"
+    song.findById(id)
+    .exec()
+    .then(doc =>{
+       console.log(doc);
+       if(doc)
+       {
+        res.status(200).json({doc});
+       }
+       else{
+        res.status(404).json({reponse : 'Record Found Null' });  
+       }
+       
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({err:err});
     });
 });
 
-router.delete('/',(req,res,next)=>{
-    res.status(200).json({
-      message : "Product / Delete Request"
+
+// http://localhost:3000/product/{id}   Patch Method
+router.patch('/:productid',function(req, res) {
+//data format to accept
+/*
+Data Format for client 
+{
+    "song": "Amar Sombol",
+    "singer_id": 17,
+    "song_type": "Folk Song",
+    "song_duration": 5.9
+}
+*/
+ 
+ const id = req.params.productid;
+ //const props = req.body;
+ const props = {
+    songname : req.body.song,
+    singer_id : req.body.singer_id,
+    song_type : req.body.song_type,
+    song_duration : req.body.song_duration
+  };
+
+ song.update({_id: id}, props, function(err, raw) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(raw);
+  });
+});
+
+// Delete api:
+// http://http://localhost:3000/product/   Delete Method
+router.delete('/:productid',(req,res,next)=>{
+    const id = req.params.productid;
+    song.remove({_id : id})
+    .exec()
+    .then(doc =>{
+       console.log(doc);
+       res.status(200).json({reponse : 'Document Deleted'});
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({err:err});
     });
 });
 
